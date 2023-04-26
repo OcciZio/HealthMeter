3b0398f74771fad060d8a4ea0ef588a76d40834a
OcciZio <OcciZioLive@gmail.com>
2023-04-26 04:16:07 -0400

    Add existing file

---
+++ InputData.js
@@ -0,0 +1,10 @@
+// InputData.js
+const calories = document.getElementById("calories").value;
+const fiber = document.getElementById("fiber").value;
+const protein = document.getElementById("protein").value;
+
+module.exports = {
+  calories,
+  fiber,
+  protein
+};
